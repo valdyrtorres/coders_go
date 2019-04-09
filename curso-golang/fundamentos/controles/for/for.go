1830865973194f6f614f3512f8f65d8ea57559ac
Valdir Torres Borges <f556085@corp.caixa.gov.br>
2019-04-09 11:35:31 -0300

    Aula 28

---
+++ curso-golang/fundamentos/controles/for/for.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// Somente existe o for como estrutura de repetição
+
+	// Usar o for como o while
+	i := 1
+	for i <= 10 { // não tem while em Go
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println("\nMisturando...")
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Print("Par ")
+		} else {
+			fmt.Print("Impar ")
+		}
+	}
+
+	for {
+		// laço infinito
+		fmt.Println("Para sempre...")
+		time.Sleep(time.Second * 5)
+	}
+
+	// Veremos o for each no capítulo de array
+}
