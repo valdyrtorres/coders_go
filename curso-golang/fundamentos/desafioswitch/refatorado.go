5dfca92e62750a803e5b9192bccb8f4cdfbd47ec
Valdir Torres Borges <f556085@corp.caixa.gov.br>
2019-04-10 15:22:44 -0300

    Aula 30

--- curso-golang/fundamentos/desafioswitch/refatorado.go
+++ curso-golang/fundamentos/desafioswitch/refatorado.go
@@ -3,22 +3,18 @@ package main
 import "fmt"
 
 func notaParaConceito(n float64) string {
-	if n >= 9 && n <= 10 {
-		return "A"
-	} else if n >= 8 && n < 9 {
-		return "B"
-	} else if n >= 5 && n < 8 {
-		return "C"
-	} else if n >= 3 && n < 5 {
-		return "D"
-	} else {
-		return "E"
-	}
 
 	switch {
 	case n >= 9 && n <= 10:
+		return "A"
 	case n >= 8 && n < 9:
-	case 
+		return "B"
+	case n >= 5 && n < 8:
+		return "C"
+	case n >= 3 && n < 5:
+		return "D"
+	default:
+		return "E"
 	}
 }
 
